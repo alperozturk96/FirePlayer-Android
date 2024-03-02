d8a1f5f47fd6ad7c36b955ef85a912372901802f
alperozturk <alper_ozturk@proton.me>
2024-03-02 15:23:22 +0100

    UI Fixes

--- app/src/main/java/com/coolnexttech/fireplayer/ui/components/button/Buttons.kt
+++ app/src/main/java/com/coolnexttech/fireplayer/ui/components/button/Buttons.kt
@@ -3,29 +3,16 @@ package com.coolnexttech.fireplayer.ui.components.button
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.coolnexttech.fireplayer.ui.theme.AppColors
-
-@Composable
-fun DialogButton(id: Int, action: () -> Unit) {
-    Button(action) {
-        Text(
-            text = stringResource(id),
-            color = AppColors.textColor
-        )
-    }
-}
 
 @Composable
 fun ActionIconButton(id: Int, modifier: Modifier = Modifier, action: () -> Unit) {
--- app/src/main/java/com/coolnexttech/fireplayer/ui/home/dialog/SortOptionsAlertDialog.kt
+++ app/src/main/java/com/coolnexttech/fireplayer/ui/home/dialog/SortOptionsAlertDialog.kt
@@ -1,25 +1,19 @@
 package com.coolnexttech.fireplayer.ui.home.dialog
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.coolnexttech.fireplayer.R
 import com.coolnexttech.fireplayer.model.SortOptions
-import com.coolnexttech.fireplayer.ui.components.Drawable
-import com.coolnexttech.fireplayer.ui.components.HeadlineSmallText
-import com.coolnexttech.fireplayer.ui.components.button.DialogButton
-import com.coolnexttech.fireplayer.ui.theme.AppColors
-import com.coolnexttech.fireplayer.utils.extensions.VSpacing16
-import com.coolnexttech.fireplayer.utils.extensions.VSpacing8
 
 @Composable
 fun SortOptionsAlertDialog(
@@ -29,44 +23,29 @@ fun SortOptionsAlertDialog(
     Dialog({ dismiss() }) {
         Column(
             modifier = Modifier
-                .width(400.dp)
-                .padding(16.dp)
-                .clip(RoundedCornerShape(30.dp))
-                .background(AppColors.alternateBackground),
-            verticalArrangement = Arrangement.Center,
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            VSpacing16()
-
-            Drawable(R.drawable.ic_fire, tint = AppColors.red)
-
-            VSpacing16()
-
-            HeadlineSmallText(R.string.home_sort_dialog_title)
-
-            VSpacing8()
-
-            DialogButton(R.string.home_sort_dialog_sort_by_title_a_z) {
-                sort(SortOptions.AToZ)
+            FilledTonalButton(onClick = { sort(SortOptions.AToZ) }) {
+                Text(text = stringResource(id = R.string.home_sort_dialog_sort_by_title_a_z))
             }
 
-            DialogButton(R.string.home_sort_dialog_sort_by_title_z_a) {
-                sort(SortOptions.ZToA)
+            FilledTonalButton(onClick = { sort(SortOptions.ZToA) }) {
+                Text(text = stringResource(id = R.string.home_sort_dialog_sort_by_title_z_a))
             }
 
-            DialogButton(R.string.home_sort_dialog_sort_by_new_to_old_title) {
-                sort(SortOptions.NewToOld)
+            FilledTonalButton(onClick = { sort(SortOptions.NewToOld) }) {
+                Text(text = stringResource(id = R.string.home_sort_dialog_sort_by_new_to_old_title))
             }
 
-            DialogButton(R.string.home_sort_dialog_sort_by_old_to_new_title) {
-                sort(SortOptions.OldToNew)
+            FilledTonalButton(onClick = { sort(SortOptions.OldToNew) }) {
+                Text(text = stringResource(id = R.string.home_sort_dialog_sort_by_old_to_new_title))
             }
 
-            DialogButton(R.string.common_cancel) {
-                dismiss()
+            TextButton(onClick = { dismiss() }) {
+                Text(text = stringResource(id = R.string.common_cancel))
             }
-
-            VSpacing16()
         }
     }
 }
