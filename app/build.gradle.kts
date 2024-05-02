f61a199d69e11beb3a92f1f747cfca47d4f0631a
alperozturk <alper_ozturk@proton.me>
2024-05-02 12:16:24 +0200

    Fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.coolnexttech.fireplayer"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.5"
+        versionCode = 3
+        versionName = "0.6"
 
         vectorDrawables {
             useSupportLibrary = true
